25b2aa2be02f4bd29603c543403a8344b0696e41
fattycat98 <102767090+fattycat98@users.noreply.github.com>
2022-12-09 04:45:45 -0500

    Add files via upload

--- src/europevanilla_model.cpp
+++ src/europevanilla_model.cpp
@@ -1,39 +1,39 @@
 #include <algorithm>
-#include <binary_tree_modern.h>
-#include <bsm.h>
-#include <cmath>
 #include <europevanilla_model.h>
+#include <bsm.h>
 #include <europevanilla_payoff.h>
+#include <binary_tree_modern.h>
+#include <cmath>
 #include <iomanip>
 #include <iostream>
-#include <random>
 #include <vector>
+#include <random>
 
 namespace LibOptions {
 
 EuropeVanillaModel::EuropeVanillaModel(const EuropeVanillaModelConfig& config)
     : d_config(config)
 {}
-double EuropeVanillaModel::calc_value()
-{
+double EuropeVanillaModel::calc_value() {
     std::vector<std::vector<double>> paths;
     std::vector<double> payoffs;
-    if (d_config.pricingmodeltype == "bsm") {
+    if (d_config.pricingmodeltype == "bsm") { 
         BSMConfig config;
-        config.d_origPrice = d_config.d_origPrice;
-        config.d_volatility = d_config.d_volatility->local_vol(0.0, 0.0);
+        config.d_origPrice=d_config.d_origPrice;
+        config.d_volatility = d_config.d_volatility.local_vol(0.0,0.0);
         config.d_riskFreeRate = d_config.d_riskFreeRate;
-        config.d_strikePrice = d_config.d_strikePrice;
-        config.d_time = d_config.d_time;
-        config.d_dividend = d_config.d_dividend;
-        config.samplesize = d_config.samplesize;
+        config.d_strikePrice=d_config.d_strikePrice;
+        config.d_time=d_config.d_time;
+        config.d_dividend=d_config.d_dividend;
+        config.samplesize =d_config.samplesize;
         BSMPath bsmpath(config);
         paths = bsmpath.generateStockPaths();
         EuroVanillaPayoffConfig payoffconfig;
-        payoffconfig.d_strikePrice = d_config.d_strikePrice;
+        payoffconfig.d_strikePrice=d_config.d_strikePrice;
         payoffconfig.iscall = d_config.iscall;
         EuroVanillaPayoff evpayoff(payoffconfig);
         payoffs = evpayoff.calc_payoff(paths);
+        
     }
     else if (d_config.pricingmodeltype == "binomial tree") {
         BinaryTreeModernConfig config;
@@ -44,12 +44,13 @@ double EuropeVanillaModel::calc_value()
         config.d_time = d_config.d_time;
         config.d_dividend = d_config.d_dividend;
         config.d_steps = d_config.samplesize;
-
-        BinaryTreeModern btpath(config);
+        BinaryTreeModern btpath(BinaryTreeModernConfig);
         auto result = btpath.generateStockPaths();
-        paths = result[0];
+        auto path_before_cleanse = result[0];
+        for (int i = 0; i < path_before_cleanse.size(); i++) {
+            paths.push_back({path_before_cleanse[i]});
+        }
         auto probs = result[1];
-
         EuroVanillaPayoffConfig payoffconfig;
         payoffconfig.d_strikePrice = d_config.d_strikePrice;
         payoffconfig.iscall = d_config.iscall;
@@ -63,9 +64,9 @@ double EuropeVanillaModel::calc_value()
         throw new std::exception;
     }
     double sum = 0.0;
-    double discount = exp(-d_config.d_riskFreeRate * d_config.d_time);
-    for (int i = 0; i < payoffs.size(); i++) { sum += payoffs[i]; }
+    double discount = exp(-d_config.d_riskFreeRate*d_config.d_time);
+    for (int i = 0; i < payoffs.size(); i++) { sum += payoffs[i] }
     return sum * discount / payoffs.size();
 }
 
-} // namespace LibOptions
+}
