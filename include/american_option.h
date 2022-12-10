5af935a237db0aa9e99730b833edb3a46d8978ac
yzl1998 <zy2496@columbia.edu>
2022-12-10 04:32:06 -0500

    Complete

--- include/american_option.h
+++ include/american_option.h
@@ -1,14 +1,32 @@
 #ifndef LIBOPTIONS_AMERICAN_OPTION_H
 #define LIBOPTIONS_AMERICAN_OPTION_H
 
+#include <vector>
+
 namespace LibOptions {
 class AmericanOption {};
+
+
+struct AmericanOptionConfig{
+    virtual ~AmericanOptionConfig() = default;
+};
+
+struct AmericanPutOptionConfig : AmericanOptionConfig{
+    double K;
+};
+
+double OptionPayoff(int option, double S, double K);
 double PutOptionPayoff(double S, double K);
 double CallOptionPayoff(double S, double K);
-double DPayoff(double S, double K);
-double D2Payoff(double S, double K);
-double SmoothDigit(double S, double K);
-double SmoothDirac(double S, double K);
+//double AmericanOptionPayoff(const std::vector<std::vector<double>> stocksPricePaths,
+//                            int timestamp,
+//                            int option,
+//                            AmericanOptionConfig &config);
+//
+//double AmericanPutOptionPayoff(const std::vector<std::vector<double>> stocksPricePaths,
+//                               int timestamp,
+//                               AmericanPutOptionConfig &config);
+
 } // namespace LibOptions
 
 #endif // LIBOPTIONS_AMERICAN_OPTION_H
--- src/american_option.cpp
+++ src/american_option.cpp
@@ -1,8 +1,19 @@
+#include <iostream>
 #include <algorithm>
+#include <vector>
 #include <american_option.h>
 #include <cmath>
 
 namespace LibOptions {
+
+double OptionPayoff(int option, double S, double K) {
+    if (option == 1) {
+        return PutOptionPayoff(S, K);
+    } else {
+        return CallOptionPayoff(S, K);
+    }
+}
+
 double PutOptionPayoff(double S, double K)
 {
     return std::max(K - S, 0.0);
@@ -13,26 +24,26 @@ double CallOptionPayoff(double S, double K)
     return std::max(S - K, 0.0);
 }
 
-double DPayoff(double S, double K)
-{
-    return SmoothDigit(S, K);
-}
+//double AmericanOptionPayoff(const std::vector<std::vector<double>> stocksPricePaths,
+//                            int timestamp,
+//                            int option,
+//                            AmericanOptionConfig &config)
+//{
+//    if (option == 1) {
+//        AmericanPutOptionConfig *americanPutOptionConfig = dynamic_cast<AmericanPutOptionConfig*>(&config);
+//        AmericanPutOptionPayoff(stocksPricePaths, timestamp, *americanPutOptionConfig);
+//    }
+//
+//}
+//
+//double AmericanPutOptionPayoff(const std::vector<std::vector<double>> stocksPricePaths,
+//                               int timestamp,
+//                               AmericanPutOptionConfig &config)
+//{
+//    double S = stocksPricePaths.at(0).at(timestamp);
+//    double K = config.K;
+//    std::cout << K << std::endl;
+//    return std::max(result, 0.0);
+//}
 
-double D2Payoff(double S, double K)
-{
-    return SmoothDirac(S, K);
-}
-
-double SmoothDigit(double S, double K)
-{
-    double a = 0.00001;
-    return -1.0 / (1.0 + exp(-(K - S) / a));
-}
-
-double SmoothDirac(double S, double K)
-{
-    double a = 0.00001;
-    return std::exp(-(K - S) / a)
-           / (a * std::pow(1.0 + std::exp(-(K - S) / a), 2));
-}
 } // namespace LibOptions
