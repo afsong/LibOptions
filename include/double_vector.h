583eaab10c61af46a0304d2c996c97a5561ee365
Annie Song <afsong2018@gmail.com>
2022-12-10 02:53:37 -0500

    force template to appear on its own line

--- include/double_vector.h
+++ include/double_vector.h
@@ -5,7 +5,8 @@
 
 namespace LibOptions {
 
-template<typename T> class DoubleVector {
+template<typename T>
+class DoubleVector {
     std::vector<T> d_data;
     size_t d_index;
 
@@ -19,23 +20,27 @@ public:
     T& get(int i, int j);
 };
 
-template<typename T> DoubleVector<T>::DoubleVector(int r, int c)
+template<typename T>
+DoubleVector<T>::DoubleVector(int r, int c)
     : d_data(r * c, T()), d_index(0), d_numRows(r), d_numCols(c)
 {}
 
-template<typename T> void DoubleVector<T>::set(int i, int j, const T& val)
+template<typename T>
+void DoubleVector<T>::set(int i, int j, const T& val)
 {
     int index = i * d_numCols + j;
     d_data[index] = val;
 }
 
-template<typename T> const T& DoubleVector<T>::get(int i, int j) const
+template<typename T>
+const T& DoubleVector<T>::get(int i, int j) const
 {
     int index = i * d_numCols + j;
     return d_data[index];
 }
 
-template<typename T> T& DoubleVector<T>::get(int i, int j)
+template<typename T>
+T& DoubleVector<T>::get(int i, int j)
 {
     int index = i * d_numCols + j;
     return d_data[index];
