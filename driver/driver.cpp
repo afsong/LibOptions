9ce00789f250e829656557e476163d31126799d6
yzl1998 <zy2496@columbia.edu>
2022-12-10 04:55:20 -0500

    Use suggested random library

--- driver/driver.cpp
+++ driver/driver.cpp
@@ -1,18 +1,20 @@
-#include <cstdlib>
-#include <ctime>
 #include <vector>
+#include <random>
 #include <longstaff_schwartz_algo.h>
 
 int main(int argc, char* argv[])
 {
-    srand(time(NULL));
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<double> dist(1.0, 10000.0);
+    int backwardSeed = int(dist(mt));
+    int forwardSeed = int(dist(mt));
 
-    std::vector<std::vector<double>> coeffs;
-    LibOptions::LongstaffConfig longstaffConfig;
+    LibOptions::LongstaffConfig longstaffConfig{};
     longstaffConfig.backwardPathsNum = 500000;
-    longstaffConfig.backwardSeed = std::rand();
+    longstaffConfig.backwardSeed = backwardSeed;
     longstaffConfig.forwardPathsNum = 500000;
-    longstaffConfig.forwardSeed = std::rand();
+    longstaffConfig.forwardSeed = forwardSeed;
     longstaffConfig.timestampNum = 100;
     longstaffConfig.S0 = 1.0;
     longstaffConfig.K = 1.0;
@@ -21,9 +23,10 @@ int main(int argc, char* argv[])
     longstaffConfig.sigma = 0.20;
     longstaffConfig.leastSquaresOrder = 10;
     longstaffConfig.plotGraphs = true;
-    LibOptions::LongstaffSchwartzAlgo longstaffSchwartzAlgo
-        = LibOptions::LongstaffSchwartzAlgo(longstaffConfig);
+
+    auto longstaffSchwartzAlgo = LibOptions::LongstaffSchwartzAlgo(longstaffConfig);
+
+    std::vector<std::vector<double>> coeffs;
     longstaffSchwartzAlgo.BackwardFit(coeffs);
     longstaffSchwartzAlgo.ForwardEvaluate(coeffs);
-
 }
